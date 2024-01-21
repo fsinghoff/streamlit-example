f691243ff71838ec19a9084e970a34674705c89c
fsinghoff <fsinghoff@gmail.com>
2024-01-21 05:57:42 +0000

    foot

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,23 +47,6 @@ import streamlit_pydantic as sp
 from typing import Optional
 from datetime import date
 
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            footer:after {
-            content:'goodbye'; 
-            visibility: visible;
-            display: block;
-            position: relative;
-            #background-color: red;
-            padding: 5px;
-            top: 2px;
-            }
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
-
 class CustomerData(BaseModel):
     customer_group_1: str
     customer_group_2: str
